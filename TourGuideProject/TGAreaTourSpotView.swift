fbcfab0c0cb80d474297b22755d8e9b7915900a0
yoosa3004Deali <yoosa2004@naver.com>
2020-07-14 14:23:47 +0800

    관광지 제목 라벨 + KingFisher를 이용해 이미지 띄우기 성공. 데이터 로드 후 내용채우기만 남은 상태

--- TourGuideProject/TGAreaTourSpotView.swift
+++ TourGuideProject/TGAreaTourSpotView.swift
@@ -66,6 +66,12 @@ extension TGAreaTourSpotView: UICollectionViewDataSource, UICollectionViewDelega
     // 여행장소들이 들어갈 셀의 사이즈
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: self.frame.width/2, height: self.frame.height/2)
-        
+
+        /*
+         
+         https://firebasestorage.googleapis.com/v0/b/honggun-blog.appspot.com/o/%E1%84%91%E1%85%B5%E1%84%8F%E1%85%A1%E1%84%8E%E1%85%B2.png?alt=media&token=68c2ffff-81a5-4db9-a67e-b776242cea02
+         */
+
+
     }
 }
--- TourGuideProject/TGTourSpotCell.swift
+++ TourGuideProject/TGTourSpotCell.swift
@@ -9,13 +9,19 @@
 import UIKit
 import Then
 import SnapKit
+import Kingfisher
 
 class TGTourSpotCell: UICollectionViewCell {
 
     // 관광지 이미지
+    var imageView = UIImageView().then {
+        $0.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.contentMode = .scaleAspectFill
+    }
     
     // 관광지 제목
-    let titleLabel = UILabel().then {
+    var titleLabel = UILabel().then {
         $0.text = "관광지 제목"
         $0.textAlignment = .center
         $0.font = UIFont.systemFont(ofSize: 20, weight: .bold)
@@ -40,6 +46,20 @@ class TGTourSpotCell: UICollectionViewCell {
             
             make.centerX.equalToSuperview()
             make.bottom.equalTo(-40)
+        }
+        
+        self.addSubview(imageView)
+        let url =
+            
+            URL(string: "https://firebasestorage.googleapis.com/v0/b/honggun-blog.appspot.com/o/%E1%84%91%E1%85%B5%E1%84%8F%E1%85%A1%E1%84%8E%E1%85%B2.png?alt=media&token=68c2ffff-81a5-4db9-a67e-b776242cea02")
+        
+        let processor = DownsamplingImageProcessor(size: imageView.bounds.size)
+        imageView.kf.setImage(with: url, options: [.processor(processor)])
+        //imageView.image = UIImage(named: "12.png")
+        imageView.snp.makeConstraints { (make) -> Void in
+            
+            make.centerX.equalToSuperview()
+            make.top.equalTo(10)
         }
     }
 
