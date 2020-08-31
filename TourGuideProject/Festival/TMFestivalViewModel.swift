012673fffd01e25e186192191774cee454da103b
yoosa3004Deali <yoosa3004@deali.net>
2020-08-31 16:57:28 +0900

    Observable에 filter, map, ifEmpty 절 추가! -> 코드가 훨씬 깔끔해짐!

--- TourGuideProject/Festival/TMFestivalViewModel.swift
+++ TourGuideProject/Festival/TMFestivalViewModel.swift
@@ -26,90 +26,88 @@ class TMFestivals: CMNetworking {
     }
     
     /*
-    override func requestAPI(update: @escaping(_ update: [Any]?) -> Void){
-        super.request(requestParam: getParam()) { (response:Any?) in
-            
-            if let result = Mapper<FestivalResponse>().map(JSONObject: response) {
-                if result.response?.head?.resultMsg == "OK" {
-                    if let apiResult = result.response?.body?.items?.item {
-                        update(self.sortByStartDate(apiResult))
-                    } else {
-                        tgLog("Festival Data load Failed")
-                        update(nil)
-                    }
-                } else {
-                    tgLog("Festival Data loading Message is not OK")
-                    tgLog(result.response?.head?.resultMsg)
-                    update(nil)
-                }
-            } else {
-                tgLog("Festival Data Mapping Failed")
-                update(nil)
-            }
-        }
-    }
+     override func requestAPI(update: @escaping(_ update: [Any]?) -> Void){
+     super.request(requestParam: getParam()) { (response:Any?) in
+     
+     if let result = Mapper<FestivalResponse>().map(JSONObject: response) {
+     if result.response?.head?.resultMsg == "OK" {
+     if let apiResult = result.response?.body?.items?.item {
+     update(self.sortByStartDate(apiResult))
+     } else {
+     tgLog("Festival Data load Failed")
+     update(nil)
+     }
+     } else {
+     tgLog("Festival Data loading Message is not OK")
+     tgLog(result.response?.head?.resultMsg)
+     update(nil)
+     }
+     } else {
+     tgLog("Festival Data Mapping Failed")
+     update(nil)
+     }
+     }
+     }
      */
     
     override func requestAPI(update: @escaping(_ update: [Any]?) -> Void) {
         
         let observable = super.request(requestParam: getParam())
-        _ = observable.subscribe { event in
-            switch event {
-            case .next(let response):
-                if let apiResult = Mapper<FestivalResponse>().map(JSONObject: response) {
-                    if apiResult.response?.head?.resultMsg == "OK" {
-                        if let item = apiResult.response?.body?.items?.item {
-                            update(self.sortByStartDate(item))
-                        } else {
-                            tgLog("Festival Data load Failed")
-                            update(nil)
-                        }
+        _ = observable
+            .map { json in Mapper<FestivalResponse>().map(JSONObject: json) }
+            .filter { json in json?.response?.head?.resultMsg == "OK" }
+            .map { result in result?.response?.body?.items?.item }
+            .ifEmpty(default: nil)
+            .subscribe { event in
+                switch event {
+                case .next(let result):
+                    if let result = result {
+                        update(self.sortByStartDate(result))
                     }
+                    break
+                case .error(let err):
+                    tgLog(err)
+                    update(nil)
+                    break
+                case .completed:
+                    break
                 }
-                break
-            case .error(let err):
-                tgLog(err)
-                update(nil)
-                break
-            case .completed:
-                break
-            }
         }
     }
-
-override func getParam() -> Dictionary<String, Any> {
     
-    var param = super.getParam()
-    
-    if let validStartDate = eventStartDate {
-        param.updateValue(validStartDate, forKey: "eventStartDate")
-    }
-    
-    return param
-}
-
-// MARK: API로부터 받은 데이터를 날짜순으로 정렬 후 월 단위로 쪼개어 테이블 뷰 데이터에 넣어줄 행사 정보 배열 생성 (1월 행사, 2월 행사, 3월 행사, ...)
-func sortByStartDate(_ targetArr: Array<FestivalInfo>) -> [[FestivalInfo]] {
-    
-    var sortedArr = Array(repeating: [FestivalInfo](), count: 12)
-    
-    // 월 단위로 쪼개어 적재
-    for idx in sortedArr.indices {
+    override func getParam() -> Dictionary<String, Any> {
         
-        let month = String.init(format: "%02d", idx+1)
+        var param = super.getParam()
         
-        let filteredByMonth = targetArr.filter {
-            
-            if let startDate = $0.eventstartdate {
-                return startDate >= Int("2020" + month + "01") ?? Int.max && startDate <= Int("2020" + month + "31") ?? Int.min
-            }
-            
-            return false
+        if let validStartDate = eventStartDate {
+            param.updateValue(validStartDate, forKey: "eventStartDate")
         }
         
-        sortedArr[idx].append(contentsOf: filteredByMonth)
+        return param
     }
     
-    return sortedArr
-}
+    // MARK: API로부터 받은 데이터를 날짜순으로 정렬 후 월 단위로 쪼개어 테이블 뷰 데이터에 넣어줄 행사 정보 배열 생성 (1월 행사, 2월 행사, 3월 행사, ...)
+    func sortByStartDate(_ targetArr: Array<FestivalInfo>) -> [[FestivalInfo]] {
+        
+        var sortedArr = Array(repeating: [FestivalInfo](), count: 12)
+        
+        // 월 단위로 쪼개어 적재
+        for idx in sortedArr.indices {
+            
+            let month = String.init(format: "%02d", idx+1)
+            
+            let filteredByMonth = targetArr.filter {
+                
+                if let startDate = $0.eventstartdate {
+                    return startDate >= Int("2020" + month + "01") ?? Int.max && startDate <= Int("2020" + month + "31") ?? Int.min
+                }
+                
+                return false
+            }
+            
+            sortedArr[idx].append(contentsOf: filteredByMonth)
+        }
+        
+        return sortedArr
+    }
 }
--- TourGuideProject/TourSpot/Model/TMTourSpotViewModel.swift
+++ TourGuideProject/TourSpot/Model/TMTourSpotViewModel.swift
@@ -18,59 +18,57 @@ class TMTourSpot: CMNetworking {
     
     override var APIKey: String {
         let key = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?serviceKey="
-                + serviceKey
-                + "&MobileApp=AppTest&MobileOS=IOS&listYN=Y&_type=json&contentTypeId=12"
+            + serviceKey
+            + "&MobileApp=AppTest&MobileOS=IOS&listYN=Y&_type=json&contentTypeId=12"
         return key
     }
     
     /*
-    override func requestAPI(update: @escaping(_ update: [Any]?) -> Void){
-        
-        super.request(requestParam: getParam()) { (response:Any?) in
-            if let result = Mapper<TourSpotResponse>().map(JSONObject: response) {
-                if result.response?.head?.resultMsg == "OK" {
-                    if let apiResult = result.response?.body?.items?.item {
-                        update(apiResult)
-                    } else {
-                        tgLog("Tour Data load Failed")
-                        update(nil)
-                    }
-                } else {
-                    tgLog("Tour Data LoadingMessage is not OK")
-                    update(nil)
-                }
-            } else {
-                tgLog("Tour Data Mapping Failed")
-                update(nil)
-            }
-        }
-    }
+     override func requestAPI(update: @escaping(_ update: [Any]?) -> Void){
+     
+     super.request(requestParam: getParam()) { (response:Any?) in
+     if let result = Mapper<TourSpotResponse>().map(JSONObject: response) {
+     if result.response?.head?.resultMsg == "OK" {
+     if let apiResult = result.response?.body?.items?.item {
+     update(apiResult)
+     } else {
+     tgLog("Tour Data load Failed")
+     update(nil)
+     }
+     } else {
+     tgLog("Tour Data LoadingMessage is not OK")
+     update(nil)
+     }
+     } else {
+     tgLog("Tour Data Mapping Failed")
+     update(nil)
+     }
+     }
+     }
      */
     
     override func requestAPI(update: @escaping(_ update: [Any]?) -> Void) {
         
         let observable = super.request(requestParam: getParam())
-        _ = observable.subscribe { event in
-            switch event {
-            case .next(let response):
-                if let apiResult = Mapper<TourSpotResponse>().map(JSONObject: response) {
-                    if apiResult.response?.head?.resultMsg == "OK" {
-                        if let item = apiResult.response?.body?.items?.item {
-                            update(item)
-                        } else {
-                            tgLog("Festival Data load Failed")
-                            update(nil)
-                        }
+        _ = observable
+            .map { json in Mapper<TourSpotResponse>().map(JSONObject: json)}
+            .filter { json in json?.response?.head?.resultMsg == "OK" }
+            .map { result in result?.response?.body?.items?.item }
+            .ifEmpty(default: nil)
+            .subscribe { event in
+                switch event {
+                case .next(let result):
+                    if let result = result {
+                        update(result)
                     }
+                    break
+                case .error(let err):
+                    tgLog(err)
+                    update(nil)
+                    break
+                case .completed:
+                    break
                 }
-                break
-            case .error(let err):
-                tgLog(err)
-                update(nil)
-                break
-            case .completed:
-                break
-            }
         }
     }
     
